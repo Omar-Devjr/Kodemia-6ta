feb6f57c68aa6c688871391372976d02cbdb175f
omar <ungober@gmail.com>
2020-02-04 20:53:22 -0600

    Ejercicio-Conversor de divisas :octocat:

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
  
 console.log('Hello Koders')
+
+//Pesos a dolares
+
+var amountMXN = 500
+var MXNToUSD = 19.49
+
+console.log('Tengo' + ' ' + amountMXN/MXNToUSD + ' ' + 'dolares')
+
+//Pesos a Euros
+var EURToMXN = 21
+console.log('Tengo' + ' ' + amountMXN/EURToMXN + ' ' + 'euros')
+
+//Pesos a Pesos colombianos
+var CUCToMXN = 12
+console.log ('Tengo' + ' ' + amountMXN/CUCToMXN + ' ' + 'pesos cubanos') 
